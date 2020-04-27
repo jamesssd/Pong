25b937fe192a66ee7d540c8ea89016ae61bb8a9d
James Dizon <dizonjerlin@gmail.com>
2020-04-27 08:57:24 -0700

    Updated Sounds

--- Pong.py
+++ Pong.py
@@ -1,5 +1,6 @@
 #turtle module allows user to add basic graphics
 import turtle 
+import winsound
 
 wn = turtle.Screen()
 wn.title("Pong by @jamessd")
@@ -92,16 +93,20 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
     
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
         score_a +=1
-
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align='center', font=('Courier', 24, 'normal'))
+        
     
     if ball.xcor() < -390:
         ball.goto(0, 0)
@@ -115,11 +120,14 @@ while True:
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -40):
         ball.setx(340)
         ball.dx *=-1
-
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+        
     if (ball.xcor() <-340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -40):
         ball.setx(-340)
         ball.dx *=-1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
 
 
 
+ 
